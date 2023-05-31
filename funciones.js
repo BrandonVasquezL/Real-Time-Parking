28b687b9f269ca24133cd5f4db8faeaf14f5cafb
unknown <zs20022115@estudiantes.uv.mx>
2023-05-30 18:00:58 -0600

    front

--- funciones.js
+++ funciones.js
@@ -1,13 +1,25 @@
-const midiv = document.querySelector("#lugar");
-var distancia = 5;
+document.addEventListener("DOMContentLoaded", cambiarcolor);
 
-midiv.addEventListener("click", cambiarcolor);
+function cambiarcolor() {
+  console.log("funcionando");
 
-function cambiarcolor(){
-    console.log("funcionando");
-if(distancia < 5){
-midiv.setAttribute("bgcolor", "red");
-}else{
-    midiv.setAttribute("bgcolor", "green")
-}
+  var lugares = document.querySelectorAll(".lugar");
+
+  lugares.forEach(function(lugar) {
+    var distanciaActual = parseFloat(lugar.dataset.distancia); // Accede al atributo de datos 'distancia' y conviértelo a número
+
+    
+    lugar.style.backgroundRepeat = "no-repeat";
+    lugar.style.backgroundPosition = "center center";
+    
+    if (distanciaActual <= 5) {
+        lugar.classList.add("distancia-menor-h2");
+        lugar.style.backgroundImage = "url('pngegg.png')";
+        lugar.querySelector("h2").style.display = "none";
+    } else {
+        lugar.classList.remove("distancia-menor-h2");
+      lugar.setAttribute("style", "background-color: green;");
+      lugar.querySelector("h2").style.display = "block";
+    }
+  });
 }
