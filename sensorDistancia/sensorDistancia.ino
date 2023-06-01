ef8911c8a46bdaa929fffe3ed35eaeefba615f12
SantiagoECHM <111538733+SantiagoECHM@users.noreply.github.com>
2023-05-31 23:26:25 -0700

    MQTT

--- sensorDistancia/sensorDistancia.ino
+++ sensorDistancia/sensorDistancia.ino
@@ -1,3 +1,7 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
 const int trigPin = 5;
 const int echoPin = 18;
 
@@ -7,31 +11,131 @@ const int echoPin = 18;
 long duration;
 float distanceCm;
 
+//Conexión a la red
+const char* ssid = "SANTY";
+const char* password = "caracoles123";
+const char* mqtt_server = "192.168.0.137";
+
+//MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  /*if ((char)payload[0] == '1') {
+    digitalWrite(2, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(2, HIGH);  // Turn the LED off by making the voltage HIGH
+  }*/
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200); // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance
-  distanceCm = duration * SOUND_SPEED/2;
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
+  unsigned long now = millis();
+  if(now - lastMsg > 5000){
+    lastMsg = now;
+    ++value;
+
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
   
-  // Prints the distance in the Serial Monitor
-  Serial.print("Distance (cm): ");
-  Serial.println(distanceCm);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
   
-  delay(1000);
+    // Calculate the distance
+    distanceCm = duration * SOUND_SPEED/2;
+
+    char mensaje[100];
+    DynamicJsonDocument data(1235);
+    data["distancia"] = distanceCm;
+    serializeJson(data, mensaje);
+
+    snprintf (msg, MSG_BUFFER_SIZE, mensaje, value);
+
+    Serial.print("Publicando mensaje: ");
+    Serial.println(mensaje);
+    // Prints the distance in the Serial Monitor
+    Serial.print("Distance (cm): ");
+    Serial.println(distanceCm);
+    client.publish("/distancia", mensaje);    
+  
+    delay(1000);
+  }
 }
--- servidorAsync/servidorAsync.ino
+++ servidorAsync/servidorAsync.ino
@@ -9,51 +9,65 @@ const char* password = "caracoles123";
 AsyncWebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   conectarse();
 
-  server.on("/estacionamiento", HTTP_GET, [](AsyncWebServerRequest *request){
-    //Abrir archivo HTML
-    File archivo = SPIFFS.open("/index.html");
-    if(!SPIFFS.begin(true)){
-     Serial.println("An Error has occurred while mounting SPIFFS");
-     return;
+  server.on("/estacionamiento", HTTP_GET, [](AsyncWebServerRequest* request) {
+    if (!SPIFFS.begin(true)) {
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
     }
-    //Saber si se lee o no el HTML
-    if(!archivo){
+
+    String path = request->url();
+    if (path != "/estacionamiento" && path != "/index.html") {
+      request->send(404, "text/plain", "Página no encontrada");
+      return;
+    }
+
+    File archivo = SPIFFS.open("/index.html");
+
+    if (!archivo) {
       request->send(404, "text/plain", "Archivo no encontrado");
-    } else{
-      
+    } else {
       String html;
-      
       while (archivo.available()) {
         html += archivo.readString();
       }
       archivo.close();
-      //Enviar la respuesta a la pagina HTML al cliente
       request->send(200, "text/html", html);
-      
     }
   });
-  
-  server.begin();  
+
+  server.onNotFound([](AsyncWebServerRequest* request) {
+    if (request->url() == "/favicon.ico") {
+      request->send(404);  // Ignorar el caso del favicon
+    } else {
+      request->send(404, "text/plain", "Favicon error");
+    }
+  });
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Tu código principal aquí (si lo tienes)
 }
 
-//Saber si se conectó o no a la red
-void conectarse(){
+void conectarse() {
   WiFi.begin(ssid, password);
-  Serial.println("Estableciendo conexión con SSID "+String(ssid));  
+  Serial.println("Estableciendo conexión con SSID " + String(ssid));
 
-  while(WiFi.status() != WL_CONNECTED){
+  unsigned long inicioConexion = millis();
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println(".");
-  }  
-  Serial.print("Conectado a la red con dirección ip: ");
-  Serial.print(WiFi.localIP());
-}
+    Serial.print(".");
 
+    if (millis() - inicioConexion > 10000) {
+      Serial.println("\nError: No se pudo establecer la conexión WiFi.");
+      return;
+    }
+  }
+
+  Serial.println("\nConectado a la red con dirección IP: " + WiFi.localIP().toString());
+}
