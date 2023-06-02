41fc9ed3f9b2275198d724a46cbb5b8ec0b2e463
SantiagoECHM <111538733+SantiagoECHM@users.noreply.github.com>
2023-06-02 00:33:22 -0700

    Tres conexiones

--- css/funciones.js
+++ css/funciones.js
@@ -1,4 +1,4 @@
-var client = new Paho.MQTT.Client("", 9001, "Santiago");
+var client = new Paho.MQTT.Client("172.21.16.1", 9001, "Santiago");
 
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
@@ -9,6 +9,8 @@ function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("onConnect");
     client.subscribe("/distancia"); 
+    client.subscribe("/distancia2"); 
+    client.subscribe("/distancia3"); 
   }
 
 function onConnectionLost(responseObject) {
@@ -18,31 +20,70 @@ function onConnectionLost(responseObject) {
 }
    // called when a message arrives
 function onMessageArrived(message) {
-
   var msg = JSON.parse(message.payloadString); 
   console.log("onMessageArrived:" + message.destinationName + message.payloadString);
   var distanciaActual = msg.distancia;
+
   console.log(distanciaActual);
+  if (message.destinationName === "/distancia") {
+    var lugares = document.querySelectorAll(".lugarPrimero");
 
-  var lugares = document.querySelectorAll(".lugar");
+    lugares.forEach(function(lugar) {
+  
+      lugar.style.backgroundRepeat = "no-repeat";
+      lugar.style.backgroundPosition = "center center";
+  
+      if (distanciaActual <= 5) {
+        lugar.classList.add("distancia-menor-h2");
+        lugar.setAttribute("style", "background-color: red;");
+        lugar.style.backgroundImage = "url('css/pngegg.png')";
+        lugar.querySelector("h2").style.display = "none";
+      } else {
+        lugar.classList.remove("distancia-menor-h2");
+        lugar.setAttribute("style", "background-color: green;");
+        lugar.querySelector("h2").style.display = "block";
+      }
+    });
+  }else if(message.destinationName === "/distancia2"){
+    var lugares = document.querySelectorAll(".lugarSegundo");
 
-  lugares.forEach(function(lugar) {
+    lugares.forEach(function(lugar) {
+  
+      lugar.style.backgroundRepeat = "no-repeat";
+      lugar.style.backgroundPosition = "center center";
+  
+      if (distanciaActual <= 5) {
+        lugar.classList.add("distancia-menor-h2");
+        lugar.setAttribute("style", "background-color: red;");
+        lugar.style.backgroundImage = "url('css/pngegg.png')";
+        lugar.querySelector("h2").style.display = "none";
+      } else {
+        lugar.classList.remove("distancia-menor-h2");
+        lugar.setAttribute("style", "background-color: green;");
+        lugar.querySelector("h2").style.display = "block";
+      }
+    });
+  }else if(message.destinationName === "/distancia3"){
+    var lugares = document.querySelectorAll(".lugarSegundo");
 
-    lugar.style.backgroundRepeat = "no-repeat";
-    lugar.style.backgroundPosition = "center center";
+    lugares.forEach(function(lugar) {
+  
+      lugar.style.backgroundRepeat = "no-repeat";
+      lugar.style.backgroundPosition = "center center";
+  
+      if (distanciaActual <= 5) {
+        lugar.classList.add("distancia-menor-h2");
+        lugar.setAttribute("style", "background-color: red;");
+        lugar.style.backgroundImage = "url('css/pngegg.png')";
+        lugar.querySelector("h2").style.display = "none";
+      } else {
+        lugar.classList.remove("distancia-menor-h2");
+        lugar.setAttribute("style", "background-color: green;");
+        lugar.querySelector("h2").style.display = "block";
+      }
+    });
+  }
 
-    if (distanciaActual <= 5) {
-      lugar.classList.add("distancia-menor-h2");
-      lugar.setAttribute("style", "background-color: red;");
-      lugar.style.backgroundImage = "url('pngegg.png')";
-      lugar.querySelector("h2").style.display = "none";
-    } else {
-      lugar.classList.remove("distancia-menor-h2");
-      lugar.setAttribute("style", "background-color: green;");
-      lugar.querySelector("h2").style.display = "block";
-    }
-  });
 }
-//'{"lat": 19, "long": -91.34343, "mensaje": "hola"}'
 
   
